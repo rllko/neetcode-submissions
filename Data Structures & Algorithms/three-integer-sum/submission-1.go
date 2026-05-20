b9b5c09748c2b13b3b32f57240e94901f94f9446
Ricardo <166535715+rllko@users.noreply.github.com>
2026-05-21 00:18:01 +0100

    Update: three-integer-sum - submission-1

--- Data Structures & Algorithms/three-integer-sum/submission-1.go
+++ Data Structures & Algorithms/three-integer-sum/submission-1.go
@@ -2,27 +2,34 @@ import "slices"
 
 func threeSum(nums []int) [][]int {
     slices.Sort(nums)
+    
     res := [][]int{}
-    l:= 0
-    r:= 0
+
     for i:= 0; i < len(nums); i++ {
+        if i > 0 && nums[i-1] == nums[i] {
+            continue;
+        }
 
-        r = len(nums)-1
-        l = i+1
+        r := len(nums)-1
+        l := i+1
+
+
         for r > l {
-            val := nums[r] + nums[l]
+            val := nums[r] + nums[l] + nums[i]
 
-            if r < len(nums)-1 && (nums[r+1] == nums[r]) && nums[l-1] == nums[l] {
-                fmt.Println("NIGGER")
-                break
-            }
-
-            if val + nums[i] == 0 {
+            if val == 0 {
                 res = append(res,[]int{nums[i],nums[l],nums[r]})
                 l++
-            } 
+                r--
 
-            if val + nums[i] > 0 {
+                for l < r && nums[l] == nums[l-1] {
+                    l++
+                }
+                
+                for l < r && nums[r] == nums[r+1] {
+                    r--
+                }
+            } else if val > 0 {
                 r--
             } else {
                 l++
