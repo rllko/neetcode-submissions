d66dc35eeaccfe28546dc146452026ba990944b0
Ricardo <166535715+rllko@users.noreply.github.com>
2026-05-21 18:33:52 +0100

    Update: concatenation-of-array - submission-0

--- Data Structures & Algorithms/concatenation-of-array/submission-0.go
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.go
@@ -2,11 +2,7 @@ func getConcatenation(nums []int) []int {
     arr := make([]int,len(nums)*2)
 
     copy(arr,nums)
-    i := len(nums)
-    for _,num := range nums {
-        arr[i] = num
-        i++
-    }
+    copy(arr[len(nums):],nums)
 
     return arr
 }
