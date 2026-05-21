f5c67a985b0862445d8a3fdfd9865d87a1dd0540
Ricardo <166535715+rllko@users.noreply.github.com>
2026-05-21 04:06:01 +0100

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.go
@@ -0,0 +1,32 @@
+func trap(height []int) int {
+    res := 0
+
+	for i := range height {
+		left,right := 0,len(height)-1
+        left_max:= 0
+        right_max:= 0
+        
+        for right > left  {
+
+            if height[left] > left_max && left < i {
+                left_max = height[left]
+            } else if left < i {
+                left++
+            }
+
+            if height[right] > right_max && right > i {
+                right_max = height[right]
+            } else if right > i {             
+                right--
+            }
+        }
+
+        water := min(left_max,right_max) - height[i]
+
+        if water > 0 {
+            res += water
+        }
+	}
+
+    return res
+}
