daaea39e1698d8c59a4cccfbe7a12abc88125aa2
Ricardo <166535715+rllko@users.noreply.github.com>
2026-05-26 03:54:20 +0100

    Update submission-1.js

--- Data Structures & Algorithms/anagram-groups/submission-1.js
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -4,22 +4,18 @@ class Solution {
      * @return {string[][]}
      */
     groupAnagrams(strs) {
-        const anagramMap = {}; // This will hold our groups
+        const anagramMap = {};
 
         for (let word of strs) {
-            // 1. Sort the word to use as a key
             let sortedWord = word.split("").sort().join("");
             
-            // 2. If this sorted word isn't in our map yet, create an empty array
             if (!anagramMap[sortedWord]) {
                 anagramMap[sortedWord] = [];
             }
             
-            // 3. Push the original word into the correct group
             anagramMap[sortedWord].push(word);
         }
 
-        // 4. Return just the grouped arrays using Object.values()
         return Object.values(anagramMap);
     }
 }
